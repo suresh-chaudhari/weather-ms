5efb9b447823846ff6f5a47c2f998d3d5500bfff
Suresh Chaudhari <suresh26272@gmail.com>
2020-08-12 22:58:50 -0400

    Junit test cases added

--- src/test/java/com/loyalty/one/rest/PostRestControllerTest.java
+++ src/test/java/com/loyalty/one/rest/PostRestControllerTest.java
@@ -1,27 +1,42 @@
 package com.loyalty.one.rest;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.loyalty.one.model.request.PostRequest;
+import com.loyalty.one.model.response.PostResponse;
 import com.loyalty.one.service.PostService;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.util.NestedServletException;
+
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class PostRestControllerTest {
 
     private MockMvc mockMvc;
 
     @InjectMocks
-    private PostRestController loyaltyRestController;
+    private PostRestController postRestController;
 
     @Mock
-    private PostService productService;
+    private PostService postService;
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
@@ -31,175 +46,103 @@ public class PostRestControllerTest {
 
         MockitoAnnotations.initMocks(this);
         mockMvc = MockMvcBuilders
-                .standaloneSetup(loyaltyRestController)
+                .standaloneSetup(postRestController)
                 .build();
     }
 
-//    @Test
-//    public void saveProduct() throws Exception {
-//        Product product = new Product();
-//        product.setProductName("Keyboard");
-//        product.setQuantity(2);
-//
-//        ProductResponse expectedProductResponse = new ProductResponse();
-//        expectedProductResponse.setMessage("Congratulations ! Data inserted successfully.");
-//
-//        when(productService.persistProduct(any())).thenReturn(expectedProductResponse);
-//
-//        MvcResult mvcResult = this.mockMvc.perform(post("/v1/api/product")
-//                .content(new ObjectMapper().writeValueAsString(product))
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andReturn();
-//        ProductResponse actualProductResponse = new ObjectMapper().readValue(mvcResult.getResponse().getContentAsString(), ProductResponse.class);
-//        assertEquals(expectedProductResponse.getMessage(), actualProductResponse.getMessage());
-//    }
-//
-//    @Test
-//    public void saveProductWhenProductNameIsEmpty() throws Exception {
-//        expectedException.expect(NestedServletException.class);
-//        expectedException.expectMessage("Please Enter Product and Quantity");
-//
-//        Product product = new Product();
-//        product.setProductName(""); //empty product name
-//        product.setQuantity(2);
-//
-//        ProductResponse expectedProductResponse = new ProductResponse();
-//        expectedProductResponse.setMessage("Congratulations ! Data inserted successfully.");
-//
-//        when(productService.persistProduct(any())).thenReturn(expectedProductResponse);
-//
-//        this.mockMvc.perform(post("/v1/api/product")
-//                .content(new ObjectMapper().writeValueAsString(product))
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isBadRequest());
-//    }
-//
-//    @Test
-//    public void saveProductWhenQuantityIsZeroOrEmpty() throws Exception {
-//        expectedException.expect(NestedServletException.class);
-//        expectedException.expectMessage("Please Enter Product and Quantity");
-//
-//        Product product = new Product();
-//        product.setProductName("Keyboard");
-//        product.setQuantity(0);//set quantity zero for validation
-//
-//        ProductResponse expectedProductResponse = new ProductResponse();
-//        expectedProductResponse.setMessage("Congratulations ! Data inserted successfully.");
-//
-//        when(productService.persistProduct(any())).thenReturn(expectedProductResponse);
-//
-//        this.mockMvc.perform(post("/v1/api/product")
-//                .content(new ObjectMapper().writeValueAsString(product))
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isBadRequest());
-//    }
-//
-//    @Test
-//    public void getAllProducts() throws Exception {
-//        Product product = new Product();
-//        product.setProductName("Keyboard");
-//        product.setQuantity(2);
-//
-//        Product product1 = new Product();
-//        product1.setProductName("Mouse");
-//        product1.setQuantity(4);
-//
-//        List<Product> expectedProductList = new ArrayList<>();
-//        expectedProductList.add(product);
-//        expectedProductList.add(product1);
-//
-//        when(productService.getAllProducts()).thenReturn(expectedProductList);
-//
-//        MvcResult mvcResult = this.mockMvc.perform(get("/v1/api/product")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andReturn();
-//
-//        List<Product> actualProductList = new ObjectMapper().readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<List<Product>>(){});
-//
-//        assertEquals(expectedProductList.get(0).getProductName(), actualProductList.get(0).getProductName());
-//        assertEquals(expectedProductList.get(0).getQuantity(), actualProductList.get(0).getQuantity());
-//        assertEquals(expectedProductList.get(1).getProductName(), actualProductList.get(1).getProductName());
-//        assertEquals(expectedProductList.get(1).getQuantity(), actualProductList.get(1).getQuantity());
-//    }
-//
-//    @Test
-//    public void deleteProduct() throws Exception {
-//        Product product = new Product();
-//        product.setProductName("Keyboard");
-//        product.setQuantity(2);
-//
-//        ProductResponse expectedProductResponse = new ProductResponse();
-//        expectedProductResponse.setMessage("Congratulations ! Data deleted successfully.");
-//
-//        when(productService.deleteProductById(anyInt())).thenReturn(expectedProductResponse);
-//
-//        MvcResult mvcResult = this.mockMvc.perform(delete("/v1/api/product/{id}", 1)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andReturn();
-//
-//        ProductResponse actualProductResponse = new ObjectMapper().readValue(mvcResult.getResponse().getContentAsString(), ProductResponse.class);
-//        assertEquals(expectedProductResponse.getMessage(), actualProductResponse.getMessage());
-//    }
-//
-//    @Test
-//    public void updateProduct() throws Exception {
-//        Product product = new Product();
-//        product.setProductName("Keyboard");
-//        product.setQuantity(2);
-//
-//        ProductResponse expectedProductResponse = new ProductResponse();
-//        expectedProductResponse.setMessage("Congratulations ! Data updated successfully.");
-//
-//        when(productService.updateProductById(anyInt(), any())).thenReturn(expectedProductResponse);
-//
-//        MvcResult mvcResult = this.mockMvc.perform(put("/v1/api/product/{id}", 1)
-//                .content(new ObjectMapper().writeValueAsString(product))
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andReturn();
-//
-//        ProductResponse actualProductResponse = new ObjectMapper().readValue(mvcResult.getResponse().getContentAsString(), ProductResponse.class);
-//        assertEquals(expectedProductResponse.getMessage(), actualProductResponse.getMessage());
-//    }
-//
-//    @Test
-//    public void updateProductWhenProductNameIsEmpty() throws Exception {
-//        expectedException.expect(NestedServletException.class);
-//        expectedException.expectMessage("Please Enter Product and Quantity");
-//
-//        Product product = new Product();
-//        product.setProductName(""); //empty product name
-//        product.setQuantity(2);
-//
-//        this.mockMvc.perform(put("/v1/api/product/{id}", 1)
-//                .content(new ObjectMapper().writeValueAsString(product))
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isBadRequest());
-//    }
-//
-//    @Test
-//    public void updateProductWhenQuantityIsZeroOrEmpty() throws Exception {
-//        expectedException.expect(NestedServletException.class);
-//        expectedException.expectMessage("Please Enter Product and Quantity");
-//
-//        Product product = new Product();
-//        product.setProductName("Keyboard");
-//        product.setQuantity(0);//set quantity zero for validation
-//
-//        this.mockMvc.perform(put("/v1/api/product/{id}", 1)
-//                .content(new ObjectMapper().writeValueAsString(product))
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isBadRequest());
-//    }
+    @Test
+    public void createPost() throws Exception {
+        PostRequest postRequest = new PostRequest();
+        postRequest.setCity("Toronto");
+        postRequest.setContent("Hello");
+        postRequest.setUsername("Suresh");
+
+        PostResponse postResponse = new PostResponse();
+        postResponse.setCity("Toronto");
+        postResponse.setContent("Hello");
+        postResponse.setUsername("Suresh");
+        postResponse.setLatitude(78.02f);
+        postResponse.setLongitude(-0.13f);
+        postResponse.setTemperature(20.5f);
+        postResponse.setPostId(1);
+
+
+        when(postService.addPostData(any())).thenReturn(postResponse);
+
+        MvcResult mvcResult = this.mockMvc.perform(post("/api/v1/post")
+                .content(new ObjectMapper().writeValueAsString(postRequest))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        PostResponse actualProductResponse = new ObjectMapper().readValue(mvcResult.getResponse().getContentAsString(), PostResponse.class);
+        assertEquals(postResponse.getPostId(), actualProductResponse.getPostId());
+        assertEquals(postResponse.getCity(), actualProductResponse.getCity());
+        assertEquals(postResponse.getContent(), actualProductResponse.getContent());
+        assertEquals(postResponse.getLatitude(), actualProductResponse.getLatitude());
+        assertEquals(postResponse.getLongitude(), actualProductResponse.getLongitude());
+        assertEquals(postResponse.getTemperature(), actualProductResponse.getTemperature());
+        assertEquals(postResponse.getUsername(), actualProductResponse.getUsername());
+    }
+
+    @Test
+    public void createPostWhenCityIsEmpty() throws Exception {
+        expectedException.expect(NestedServletException.class);
+        expectedException.expectMessage("Please enter username, content and city.");
+
+        PostRequest postRequest = new PostRequest();
+//        postRequest.setCity("Toronto");
+        postRequest.setContent("Hello");
+        postRequest.setUsername("Suresh");
+
+        PostResponse postResponse = new PostResponse();
+        postResponse.setCity("Toronto");
+        postResponse.setContent("Hello");
+        postResponse.setUsername("Suresh");
+        postResponse.setLatitude(78.02f);
+        postResponse.setLongitude(-0.13f);
+        postResponse.setTemperature(20.5f);
+        postResponse.setPostId(1);
+
+
+        when(postService.addPostData(any())).thenReturn(postResponse);
+
+        this.mockMvc.perform(post("/api/v1/post")
+                .content(new ObjectMapper().writeValueAsString(postRequest))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+
+    @Test
+    public void getAllPosts() throws Exception {
+
+        PostResponse postResponse = new PostResponse();
+        postResponse.setCity("Toronto");
+        postResponse.setContent("Hello");
+        postResponse.setUsername("Suresh");
+        postResponse.setLatitude(78.02f);
+        postResponse.setLongitude(-0.13f);
+        postResponse.setTemperature(20.5f);
+        postResponse.setPostId(1);
+
+
+        when(postService.getAllPosts()).thenReturn(Collections.singletonList(postResponse));
+
+        MvcResult mvcResult = this.mockMvc.perform(get("/api/v1/post")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        List<PostResponse> actualProductResponseList = new ObjectMapper().readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<List<PostResponse>>(){});
+        PostResponse actualProductResponse = actualProductResponseList.get(0);
+        assertEquals(postResponse.getPostId(), actualProductResponse.getPostId());
+        assertEquals(postResponse.getCity(), actualProductResponse.getCity());
+        assertEquals(postResponse.getContent(), actualProductResponse.getContent());
+        assertEquals(postResponse.getLatitude(), actualProductResponse.getLatitude());
+        assertEquals(postResponse.getLongitude(), actualProductResponse.getLongitude());
+        assertEquals(postResponse.getTemperature(), actualProductResponse.getTemperature());
+        assertEquals(postResponse.getUsername(), actualProductResponse.getUsername());
+    }
+
 }
--- src/test/java/com/loyalty/one/service/PostServiceTest.java
+++ src/test/java/com/loyalty/one/service/PostServiceTest.java
@@ -1,86 +1,98 @@
 package com.loyalty.one.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.loyalty.one.model.dao.PostsDao;
+import com.loyalty.one.model.request.PostRequest;
+import com.loyalty.one.model.response.PostResponse;
 import com.loyalty.one.repository.PostRepository;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class PostServiceTest {
 
     @InjectMocks
-    private PostService productService;
+    private PostService postService;
 
     @Mock
-    private PostRepository loyaltyRepository;
+    private PostRepository postRepository;
+
+    @Mock
+    private WeatherService weatherService;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
     }
 
-//    @Test
-//    public void persistProduct() {
-//
-//        Product product = new Product();
-//        product.setProductName("Keyboard");
-//        product.setQuantity(2);
-//
-//        when(loyaltyRepository.save(any())).thenReturn(any());
-//
-//        ProductResponse productResponse = productService.persistProduct(product);
-//        assertEquals("Congratulations ! Data inserted successfully.", productResponse.getMessage());
-//    }
-//
-//    @Test
-//    public void getAllProducts() {
-//
-//        Product product = new Product();
-//        product.setProductName("Keyboard");
-//        product.setQuantity(2);
-//
-//        Product product1 = new Product();
-//        product1.setProductName("Mouse");
-//        product1.setQuantity(4);
-//
-//        List<Product> expectedProductList = new ArrayList<>();
-//        expectedProductList.add(product);
-//        expectedProductList.add(product1);
-//
-//        when(loyaltyRepository.findAll()).thenReturn(expectedProductList);
-//
-//        List<Product> actualProductList = productService.getAllProducts();
-//        assertEquals(expectedProductList.get(0).getProductName(), actualProductList.get(0).getProductName());
-//        assertEquals(expectedProductList.get(0).getQuantity(), actualProductList.get(0).getQuantity());
-//        assertEquals(expectedProductList.get(1).getProductName(), actualProductList.get(1).getProductName());
-//        assertEquals(expectedProductList.get(1).getQuantity(), actualProductList.get(1).getQuantity());
-//    }
-//
-//    @Test
-//    public void deleteProductById() {
-//
-//        Product product = new Product();
-//        product.setProductName("Keyboard");
-//        product.setQuantity(2);
-//
-//        doNothing().when(loyaltyRepository).deleteById(any());
-//
-//        ProductResponse productResponse = productService.deleteProductById(1);
-//        assertEquals("Congratulations ! Data deleted successfully.", productResponse.getMessage());
-//    }
-//
-//    @Test
-//    public void updateProductById() {
-//
-//        Product product = new Product();
-//        product.setProductName("Keyboard-Updated");
-//        product.setQuantity(3);
-//
-//        doNothing().when(loyaltyRepository).updateProduct(anyString(), anyInt(), anyInt());
-//
-//        ProductResponse productResponse = productService.updateProductById(1, product);
-//        assertEquals("Congratulations ! Data updated successfully.", productResponse.getMessage());
-//    }
+    @Test
+    public void persistPost() {
+
+        PostRequest postRequest = new PostRequest();
+        postRequest.setCity("Toronto");
+        postRequest.setContent("Hello");
+        postRequest.setUsername("Suresh");
+
+
+        PostsDao postsDao = new PostsDao();
+        postsDao.setCity("Toronto");
+        postsDao.setContent("Hello");
+        postsDao.setUsername("Suresh");
+        postsDao.setLatitude(78.02f);
+        postsDao.setLongitude(-0.13f);
+        postsDao.setTemperature(20.5f);
+        postsDao.setPostId(1);
+
+        when(postRepository.save(postsDao)).thenReturn(postsDao);
+        doNothing().when(weatherService).addWeatherInformation(any(), anyString());
+
+        PostResponse actualProductResponse = postService.addPostData(postRequest);
+        assertEquals(postsDao.getCity(), actualProductResponse.getCity());
+        assertEquals(postsDao.getContent(), actualProductResponse.getContent());
+        assertEquals(postsDao.getUsername(), actualProductResponse.getUsername());
+    }
+
+    @Test
+    public void getAllPosts() throws Exception {
+
+        PostResponse postResponse = new PostResponse();
+        postResponse.setCity("Toronto");
+        postResponse.setContent("Hello");
+        postResponse.setUsername("Suresh");
+        postResponse.setLatitude(78.02f);
+        postResponse.setLongitude(-0.13f);
+        postResponse.setTemperature(20.5f);
+        postResponse.setPostId(1);
+
+
+        when(postService.getAllPosts()).thenReturn(Collections.singletonList(postResponse));
+
+        List<PostResponse> actualProductResponseList = postService.getAllPosts();
+        PostResponse actualProductResponse = actualProductResponseList.get(0);
+
+        assertEquals(postResponse.getPostId(), actualProductResponse.getPostId());
+        assertEquals(postResponse.getCity(), actualProductResponse.getCity());
+        assertEquals(postResponse.getContent(), actualProductResponse.getContent());
+        assertEquals(postResponse.getLatitude(), actualProductResponse.getLatitude());
+        assertEquals(postResponse.getLongitude(), actualProductResponse.getLongitude());
+        assertEquals(postResponse.getTemperature(), actualProductResponse.getTemperature());
+        assertEquals(postResponse.getUsername(), actualProductResponse.getUsername());
+    }
 }
